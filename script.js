3cf9f8d11fe6a52d1d9a8ad528586826ddc9690d
HappyNatsu10 <afilakapeter@gmail.com>
2023-10-10 13:51:49 +0100

    Mobile Responsiveness

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function myFunction(smallImg) {
     var fullImg = document.getElementById("imageBox")
     fullImg.src = smallImg.src
 }
+
+const toggleBtn =document.querySelector('.search')
+        const toggleBtnIcon =document.querySelector('.search i')
+        const dropDownMenu =document.querySelector('.drop_search-bar')
+
+        toggleBtn.onclick = function () {
+            dropDownMenu.classList.toggle('open')
+            const isOpen = dropDownMenu.classList.contains('open')
+
+            toggleBtnIcon.classList = isOpen
+                ? 'fa-solid fa-xmark'
+                : 'fa-solid fa-bars'
+        }
