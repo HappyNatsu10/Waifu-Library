c3117116624b5a97ac853c23c35df9c1d4e1161c
HappyNatsu10 <afilakapeter@gmail.com>
2023-10-18 18:25:07 +0100

    Images Changes

--- script.js
+++ script.js
@@ -15,3 +15,49 @@ const toggleBtn =document.querySelector('.search')
                 ? 'fa-solid fa-xmark'
                 : 'fa-solid fa-magnifying-glass'
         }
+
+
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+ let lenghtItems = items.length - 1;
+next.onclick = function(){
+    if(active + 1 > lenghtItems){
+        active = 0;
+    } else {
+        active = active + 1;
+    }
+    reloadSlider();
+}
+
+prev.onclick = function(){
+    if(active - 1 < 0){
+        active = lenghtItems;
+    }else {
+        active = active - 1
+    }
+    reloadSlider()
+}
+
+let refreshSlider = setInterval(()=> {next.click()}, 3000);
+
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider); 
+    refreshSlider = setInterval(()=> {next.click()}, 3000);
+}
+dots.forEach((li, key) => {
+    li.addEventListener('click', function(){
+        active = key; 
+        reloadSlider();
+    })
+})
